af9bd2c6ef007286c6ecabc093daa6b7358866fb
Shashwat <shashwat.pulak@gmail.com>
2018-08-17 21:49:24 +0530

    Add passport auth strategy for login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:admin123@ds129670.mlab.com:29670/shpdbase'
+  mongoURI: 'mongodb://admin:admin123@ds129670.mlab.com:29670/shpdbase',
+  secretOrKey: 'secretKey'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 // Load User model
 const User = require('../../models/User');
+
+const keys = require('../../config/keys');
+
+const router = express.Router();
 
 /**
  * @route   GET api/users/test
@@ -70,7 +75,22 @@ router.post('/login', (req, res) => {
     // Check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: 'Success' });
+        // Create JWT Token payload
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({
+              success: true,
+              token: `Bearer ${token}`
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: 'Password Incorrect' });
       }
@@ -78,4 +98,21 @@ router.post('/login', (req, res) => {
   });
 });
 
+/**
+ * @route   POST api/users/current
+ * @desc    Return current user
+ * @access  private
+ */
+router.post(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // res.json({ msg: 'Success' });
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 module.exports = router;
