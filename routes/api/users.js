526d9251c65816d5282455ab58ec8796c36bea5d
Shashwat <shashwat.pulak@gmail.com>
2018-08-17 17:02:07 +0530

    Add user registration functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../../models/User');
 
 /**
  * @route   GET api/users/test
@@ -8,4 +13,41 @@ const router = express.Router();
  */
 router.get('/test', (req, res) => res.json({ msg: 'users works' }));
 
+/**
+ * @route   POST api/users/register
+ * @desc    Register User
+ * @access  public
+ */
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists!' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // rating
+        d: 'mm' // default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) throw err;
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 module.exports = router;
