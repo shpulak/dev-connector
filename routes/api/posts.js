4c001362464312aac6040840531b1fa14728f994
Shashwat <shashwat.pulak@gmail.com>
2018-08-18 17:55:53 +0530

    Add like and comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -108,4 +108,153 @@ router.delete(
       });
   }
 );
+
+/**
+ * @route   POST api/posts/like/:id
+ * @desc    Like a post
+ * @access  private
+ */
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id).then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            errors.alreadyliked = 'User already liked this post';
+            return ErrorHandler.badRequest(errors, res);
+          }
+
+          // Add user to likes list
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        });
+      })
+      .catch(err => {
+        errors.nopostfound = 'No post found';
+        console.log(err);
+        ErrorHandler.notFound(errors, res);
+      });
+  }
+);
+
+/**
+ * @route   POST api/posts/unlike/:id
+ * @desc    Unlike a post
+ * @access  private
+ */
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id).then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            errors.notliked = 'User has not yet liked this post';
+            return ErrorHandler.badRequest(errors, res);
+          }
+
+          // Remove user from likes list
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          post.likes.splice(removeIndex, 1);
+          post.save().then(post => res.json(post));
+        });
+      })
+      .catch(err => {
+        errors.nopostfound = 'No post found';
+        console.log(err);
+        ErrorHandler.notFound(errors, res);
+      });
+  }
+);
+
+/**
+ * @route   POST api/posts/comment/:id
+ * @desc    Add comment to post
+ * @access  private
+ */
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return ErrorHandler.badRequest(errors, res);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => {
+        errors.nopostfound = 'No post found';
+        console.log(err);
+        ErrorHandler.notFound(errors, res);
+      });
+  }
+);
+
+/**
+ * @route   DELETE api/posts/comment/:id/:comment_id
+ * @desc    Delete comment from post
+ * @access  private
+ */
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check if exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          errors.commentnotfound = 'Comment does not exists';
+          return ErrorHandler.notFound(errors, res);
+        }
+
+        // Remove the comment
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        post.comments.splice(removeIndex, 1);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => {
+        errors.nopostfound = 'No post found';
+        console.log(err);
+        ErrorHandler.notFound(errors, res);
+      });
+  }
+);
+
 module.exports = router;
