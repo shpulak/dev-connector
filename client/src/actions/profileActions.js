2829ccf4d4b7b821a55537d82f78d11cbfca9031
Shashwat <shashwat.pulak@gmail.com>
2018-09-18 02:04:46 +0530

    Add profile page components - header, about, creds

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -30,6 +30,26 @@ export const getCurrentProfile = () => dispatch => {
     });
 };
 
+// Get Profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  setAuthToken(localStorage.getItem('jwtToken'));
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res => {
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      });
+    });
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
