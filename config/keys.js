7ac5b185bfe385d08e7a2de74a8884f998f37c52
Shashwat <shashwat.pulak@gmail.com>
2018-09-20 23:18:53 +0530

    Configure env keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://admin:admin123@ds129670.mlab.com:29670/shpdbase',
-  secretOrKey: 'secretKey'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
